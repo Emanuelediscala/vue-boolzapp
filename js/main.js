ea056a2e9d6db101700c6b1aee7764ff32507ec1
Emanuele discala <manueldiscala@gmail.com>
2023-05-12 12:38:29 +0200

    added and fixed overflow

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
+            currentUser: {
+                name:"Emanuele",
+            },
             contacts: [
     {
         name: 'Michele',
@@ -164,7 +167,29 @@ createApp({
                 status: 'received'
             }
         ],
-    }
+    },
+    {
+        name: 'Michele',
+        avatar: './img/avatar_1.jpg',
+        visible: true,
+        messages: [
+            {
+                date: '10/01/2020 15:30:55',
+                message: 'Hai portato a spasso il cane?',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'Ricordati di stendere i panni',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 16:15:22',
+                message: 'Tutto fatto!',
+                status: 'received'
+            }
+        ],
+    },
 ]
         }
     },
