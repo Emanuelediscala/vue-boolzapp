5a06dc898bb3c204342d156a74630a751afe2913
Emanuele discala <manueldiscala@gmail.com>
2023-05-17 11:45:22 +0200

    added showDropdown proprerities

--- js/main.js
+++ js/main.js
@@ -5,10 +5,7 @@ createApp({
         return {
             searchContact: "",
             sentMessage: "",
-            currentUser: {
-                name: "Emanuele",
-            },
-            currentMessage:0,
+            currentMessage: 0,
             currentContact:{},
             contacts: [
                 {
@@ -20,13 +17,13 @@ createApp({
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent',
-                            showDropDown:false,
+                            showDropDown: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent',
-                            showDropDown:false,
+                            showDropDown: false,
                         },
                         {
                             date: '10/01/2020 16:15:22',
@@ -44,17 +41,20 @@ createApp({
                         {
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            showDropDown: false,
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
+                            showDropDown: false,
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            showDropDown: false,
                         }
                     ],
                 },
@@ -66,17 +66,20 @@ createApp({
                         {
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            showDropDown: false,
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            showDropDown: false,
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
-                            status: 'received'
+                            status: 'received',
+                            showDropDown: false,
                         }
                     ],
                 },
@@ -88,12 +91,16 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            showDropDown: false,
+
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            showDropDown: false,
+
                         }
                     ],
                 },
@@ -105,12 +112,15 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
-                            status: 'sent'
+                            status: 'sent',
+                            showDropDown: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
-                            status: 'received'
+                            status: 'received',
+                            showDropDown: false,
+
                         }
                     ],
                 },
@@ -122,17 +132,23 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
-                            status: 'sent'
+                            status: 'sent',
+                            showDropDown: false,
+
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
-                            status: 'received'
+                            status: 'received',
+                            showDropDown: false,
+
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
+                            status: 'sent',
+                            showDropDown: false,
+
                         }
                     ],
                 },
@@ -144,12 +160,16 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                            status: 'sent'
+                            status: 'sent',
+                            showDropDown: false,
+
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                            status: 'received'
+                            status: 'received',
+                            showDropDown: false,
+
                         }
                     ],
                 },
@@ -161,17 +181,23 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                            status: 'received'
+                            status: 'received',
+                            showDropDown: false,
+
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                            status: 'sent'
+                            status: 'sent',
+                            showDropDown: false,
+
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
-                            status: 'received'
+                            status: 'received',
+                            showDropDown: false,
+
                         }
                     ],
                 },
@@ -186,10 +212,20 @@ createApp({
         },
         // FUNZIONE PER INVIO MESSAGGI
         sendMessage() {
-            this.currentContact.messages.push({ message: this.sentMessage, status: "sent", date: new Date().toLocaleString() })
+            this.currentContact.messages.push({ 
+                message: this.sentMessage, 
+                showDropDown: false, 
+                status: "sent", 
+                date: new Date().toLocaleString() 
+            })
             const appCurrentContact = this.currentContact
             setTimeout(() => {
-                appCurrentContact.messages.push({ message: "Ok", status: "received", date: new Date().toLocaleString(),showDropDown:"false"})
+                appCurrentContact.messages.push({ 
+                    message: "Ok", 
+                    status: "received", 
+                    date: new Date().toLocaleString(), 
+                    showDropDown: false 
+                })
             }, 3 * 1000);
             this.sentMessage = "";
         },
@@ -208,20 +244,20 @@ createApp({
         },
         // PROVA MS 5
         toggleDropdown(i) {
-        this.contacts[this.currentMessage].messages[i].showDropDown = !this.contacts[this.currentMessage].messages[i].showDropDown;
-        
+            this.contacts[this.currentMessage].messages[i].showDropDown = !this.contacts[this.currentMessage].messages[i].showDropDown;
+
         },
         // FORMATTAZIONE TEMPO
         formattingTime(data) {
-            return data.split(" ")[1].substring(0,5)
+            return data.split(" ")[1].substring(0, 5)
         },
         deleteMsg(i) {
-            this.contacts[this.currentMessage].messages.splice(i,1)
+            this.contacts[this.currentMessage].messages.splice(i, 1)
         }
     },
     // DEFINISCO AL CARICAMENTO DELLA PAGINA UN DEFAULT TEMPLATE
     mounted() {
-    console.log(`the component is now mounted.`)
+        console.log(`the component is now mounted.`)
         this.currentContact = this.contacts[0]
-}
+    }
 }).mount("#app")
